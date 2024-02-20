ea18fc68ca4927b74161dc834bcf0e2bb7d51fe3
valeriaasalcedoo <153454325+valeriaasalcedoo@users.noreply.github.com>
2024-02-20 02:19:19 +0000

    coso

--- src/app.js
+++ src/app.js
@@ -5,23 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function(){
+window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-
-let dom = function(pronoun, adj, noun) {
-  let dominios = [];
-  for (let x = 0; x < pronoun.length; x++) {
-    for (let y = 0; y < adj.length; y++) {
-      for (let z = 0; z < noun.length; z++) {
-        
-        dominios.push(pronoun[x] + adj[y] + noun[z] + ".com");
+  let dom = function(pronoun, adj, noun) {
+    let dominios = [];
+    for (let x = 0; x < pronoun.length; x++) {
+      for (let y = 0; y < adj.length; y++) {
+        for (let z = 0; z < noun.length; z++) {
+          dominios.push(pronoun[x] + adj[y] + noun[z] + ".com");
+        }
       }
     }
-  }
-  console.log(dominios);
+    console.log(dominios);
+  };
+  dom(pronoun, adj, noun);
 };
-dom(pronoun, adj, noun);
-}

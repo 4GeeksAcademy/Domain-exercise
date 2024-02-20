feee2beecc72330e5ea1c0d1486612e26d3e6556
valeriaasalcedoo <153454325+valeriaasalcedoo@users.noreply.github.com>
2024-02-20 01:45:32 +0000

    cambio

--- src/app.js
+++ src/app.js
@@ -11,16 +11,17 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-let domainGenerator = function(pronoun, adj, noun) {
+let dom = function(pronoun, adj, noun) {
   let dominios = [];
   for (let x = 0; x < pronoun.length; x++) {
     for (let y = 0; y < adj.length; y++) {
       for (let z = 0; z < noun.length; z++) {
+        
         dominios.push(pronoun[x] + adj[y] + noun[z] + ".com");
       }
     }
   }
   console.log(dominios);
 };
-domainGenerator(pronoun, adj, noun);
+dom(pronoun, adj, noun);
 }

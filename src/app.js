71d3a7ec6d07d39ba5cd135870b16343308798ce
valeriaasalcedoo <153454325+valeriaasalcedoo@users.noreply.github.com>
2024-02-20 01:43:06 +0000

    dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function(){
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let domainGenerator = function(pronoun, adj, noun) {
+  let dominios = [];
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; z < noun.length; z++) {
+        dominios.push(pronoun[x] + adj[y] + noun[z] + ".com");
+      }
+    }
+  }
+  console.log(dominios);
 };
+domainGenerator(pronoun, adj, noun);
+}
